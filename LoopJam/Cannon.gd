74adfd2a9b4d5194d14e87df3abce992178471fa
Wykkid <linus@pernyer.se>
2020-10-03 12:52:23 +0200

    Trying again to commit pew pew

---
+++ LoopJam/Cannon.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+signal shoot(plasma, direction, location)
+
+var Plasma = preload("res://Plasma.tscn")
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+
+func _input(event):
+	if Input.is_action_pressed("shoot"):
+		emit_signal("shoot", Plasma, rotation, position)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
